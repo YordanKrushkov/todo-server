1f0aed52391cdeb197cbef9d530da8583597b410
Yordan Krushkov <yordankrushkov@gmail.com>
2021-05-22 12:01:22 +0100

    /serever

--- Config/config.js
+++ Config/config.js
@@ -4,11 +4,9 @@ const config = {
     development: {
         port: process.env.PORT ||80,
         dbURL: process.env.DB_URL,
-        privateKey: process.env.PRIVATE_KEY
     },
     production: {
         dbURL: process.env.DB_URL,
-        privateKey: process.env.PRIVATE_KEY
     }
 };
 
